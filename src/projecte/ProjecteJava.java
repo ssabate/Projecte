bc88fd142b56abbb5679d6d2a33c4c95db7ddbfc
Santi Sabate <SSabate@iesebre.com>
2016-11-01 11:56:22 +0100

    Tot OK

--- src/projecte/ProjecteJava.java
+++ src/projecte/ProjecteJava.java
@@ -28,7 +28,7 @@ public class ProjecteJava {
         int opcio;
 
         do {
-            System.out.println("\n\nMenú del programa.");
+            System.out.println("\n\nMenú de l'aplicació.");
             System.out.println("0. Sortir.");
             System.out.println("1. Introduïr pilot.");
             System.out.println("2. Modificar pilot.");
@@ -36,10 +36,10 @@ public class ProjecteJava {
             System.out.println("4. Llistar pilots.");
             System.out.println("5. Recuperar pilot borrat.");
             switch (opcio = ent.skip("[\r\n]*").nextInt()) {
-                case 0:
+                case 0:                             //0. Sortir
                     System.out.println("Adéu!!");
                     break;
-                case 1:
+                case 1:                             //1. Introduïr pilot
                     if (!omplit) {
                         System.out.println("\nNom:");
                         nom = ent.skip("[\r\n]*").nextLine();
@@ -60,11 +60,8 @@ public class ProjecteJava {
                         System.out.println("\nNo hi ha pilots per omplir, si vols primer borra'n.");
                     }
                     break;
-                case 2:
-                    break;
-                case 3:
+                case 2:                             //2. Modificar pilot
                     if (omplit) {
-
                         char siNo;
                         do {
                             System.out.println("\nVols vore el pilot?(S/N):");
@@ -75,6 +72,85 @@ public class ProjecteJava {
                             System.out.println("\nNom: "+nom);
                             System.out.println("Dorsal: "+dorsal);
                             System.out.println("Diners guanyats: "+dinersGuanyats);
+                            if(home) System.out.println("És home");
+                            else System.out.println("És dona");
+                      }     
+
+                        do {
+                            System.out.println("\nVols modificar el pilot?(S/N):");
+                            siNo = ent.skip("[\r\n]*").nextLine().toUpperCase().charAt(0); 
+                        } while (siNo != 'S' && siNo != 'N');
+                        if (siNo == 'S'){
+                            
+                            System.out.println("\nNom: "+nom);
+                            do {
+                                System.out.println("\nVols modificar el nom?(S/N):");
+                                siNo = ent.skip("[\r\n]*").nextLine().toUpperCase().charAt(0); 
+                            } while (siNo != 'S' && siNo != 'N');
+                            if (siNo == 'S'){
+                                System.out.print("Nou nom: ");
+                                nom = ent.skip("[\r\n]*").nextLine();
+                            }
+
+                            System.out.println("\nDorsal: "+dorsal);
+                            do {
+                                System.out.println("\nVols modificar el dorsal?(S/N):");
+                                siNo = ent.skip("[\r\n]*").nextLine().toUpperCase().charAt(0); 
+                            } while (siNo != 'S' && siNo != 'N');
+                            if (siNo == 'S'){
+                                System.out.print("Nou dorsal: ");
+                                dorsal = ent.skip("[\r\n]*").nextInt();
+                            }
+
+                            System.out.println("\nDiners guanyats: "+dinersGuanyats);
+                            do {
+                                System.out.println("\nVols modificar els diners guanyats?(S/N):");
+                                siNo = ent.skip("[\r\n]*").nextLine().toUpperCase().charAt(0); 
+                            } while (siNo != 'S' && siNo != 'N');
+                            if (siNo == 'S'){
+                                System.out.print("Nous diners guanyats: ");
+                                dinersGuanyats = ent.skip("[\r\n]*").nextDouble();
+                            }
+
+                            if(home) System.out.println("\nÉs home");
+                            else System.out.println("\nÉs dona");
+                            do {
+                                System.out.println("\nVols modificar el gènere?(S/N):");
+                                siNo = ent.skip("[\r\n]*").nextLine().toUpperCase().charAt(0); 
+                            } while (siNo != 'S' && siNo != 'N');
+                            if (siNo == 'S'){
+                                char esHome;
+                                do {
+                                    System.out.println("És home o dona?(H/D):");
+                                    esHome = ent.skip("[\r\n]*").nextLine().toUpperCase().charAt(0);
+                                } while (esHome != 'H' && esHome != 'D');
+                                home = (esHome == 'H');     //si esHome conté la 'H' home serà true i sinó false. Fa el mateix que un if_else però és molt més curt
+                                System.out.print("Nou gènere: ");
+                                if(home) System.out.println("home");
+                                else System.out.println("dona");
+                            }
+
+                            System.out.println("Pilot modificat correctament.");
+                        } else System.out.println("Pilot no modificat.");  
+                        
+                    } else {
+                        System.out.println("\nNo hi ha pilots per modificar, si vols primer crea'n.");
+                    }
+                    break;
+                case 3:                                     //3. Borrar pilot
+                    if (omplit) {
+                        char siNo;
+                        do {
+                            System.out.println("\nVols vore el pilot?(S/N):");
+                            siNo = ent.skip("[\r\n]*").nextLine().toUpperCase().charAt(0); //usem toUpperCase() que traduix el text introduït per l'usuari a majúscules, 
+                                                                                                //per tant només haurem de tractar les lletres majúscules
+                        } while (siNo != 'S' && siNo != 'N');
+                        if (siNo == 'S'){
+                            System.out.println("\nNom: "+nom);
+                            System.out.println("Dorsal: "+dorsal);
+                            System.out.println("Diners guanyats: "+dinersGuanyats);
+                            if(home) System.out.println("És home");
+                            else System.out.println("És dona");
                         }     
 
                         do {
@@ -91,16 +167,18 @@ public class ProjecteJava {
                         System.out.println("\nNo hi ha pilots per borrar, si vols primer crea'n.");
                     }
                     break;
-                case 4:
+                case 4:                                     //4. Llistar pilots
                     if (omplit) {
                         System.out.println("\nNom: "+nom);
                         System.out.println("Dorsal: "+dorsal);
                         System.out.println("Diners guanyats: "+dinersGuanyats);                        
+                        if(home) System.out.println("És home");
+                        else System.out.println("És dona");
                     } else {
                         System.out.println("\nNo hi ha pilots per mostrar, si vols primer crea'n.");
                     }                    
                     break;
-                case 5:
+                case 5:                                     //5. Recuperar pilot borrat
                     
                     if (!omplit) {
 
@@ -114,7 +192,9 @@ public class ProjecteJava {
                             System.out.println("\nNom: "+nom);
                             System.out.println("Dorsal: "+dorsal);
                             System.out.println("Diners guanyats: "+dinersGuanyats);
-                        }     
+                            if(home) System.out.println("És home");
+                            else System.out.println("És dona");
+                       }     
 
                         do {
                             System.out.println("\nVols recuperar el pilot?(S/N):");
